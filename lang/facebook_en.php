7eb92d527fd95308c48aa36fbcc99e5407031d05
gizmore <gizmore@wechall.net>
2017-07-19 22:08:00 +0200

    fixes-and-cleanup

--- lang/facebook_en.php
+++ lang/facebook_en.php
@@ -4,4 +4,6 @@ return array(
 'ft_facebook_auth' => '[%s] Continue with Facebook',
 'msg_facebook_connected' => 'You have successfully authenticated with your Facebook account.',
 'err_facebook_connect' => 'Facebook oAuth authentication process failed.',
+'msg_fb_avatar_imported' => 'Your facebook avatar image has been imported.',
+'err_fb_avatar_not_imported' => 'Your facebook avatar has not been imported.',
 );
